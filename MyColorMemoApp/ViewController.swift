8516ee26739a78f750dd62b22f2171af9224f1ea
fyky <fuyu12dmb25ms@gmail.com>
2022-01-01 23:12:38 +0900

    [Start]

---
+++ MyColorMemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyColorMemoApp
+//
+//  Created by fyky on 2022/01/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
