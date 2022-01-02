802d61a1963e90a3d382728d03208cc28cf53cf1
fyky <fuyu12dmb25ms@gmail.com>
2022-01-02 23:59:15 +0900

    [Add]Text,Date表示

---
+++ MyColorMemoApp/MemoDataModel.swift
@@ -0,0 +1,14 @@
+//
+//  MemoDataModel.swift
+//  MyColorMemoApp
+//
+//  Created by fyky on 2022/01/02.
+//
+
+import Foundation
+
+struct MemoDataModel {
+    var text: String
+    var recordDate: Date
+    
+}
